ce74cef0fe4b42c23b722d3fc372a4d444a24cf5
1900303424 <dumpasai54@outlook.com>
2022-11-21 15:00:05 +0530

    pipe |

--- db_access.py
+++ db_access.py
@@ -7,16 +7,22 @@ Created on Fri Oct 21 15:12:58 2022
 
 import pyodbc
 
-from index import sendFilePath
+from cmd_write_line import sendFileData
+
+import time
+import sys
+import win32pipe
+import win32file
+import pywintypes
 
 
-
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\DUMA1KOR\Downloads\Database3.accdb;')
+conn = pyodbc.connect(
+    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\DUMA1KOR\Downloads\Database3.accdb;')
 cursor = conn.cursor()
 # cursor.execute('select * from Sheet1 where col_name=Field1')
 cursor.execute('select Field1 from Sheet3')
 
-db_list = [] 
+db_list = []
 
 
 def fetch_data_from_db():
@@ -28,13 +34,42 @@ def fetch_data_from_db():
     #         print("Hello World")
     return db_list
 
+# resp = []
+output = []
 
-def get_template_path():
-    fs = open(r'C:\Users\DUMA1KOR\Desktop\Codes\React\Master-TARA-Creator-main\flask\FilePath.txt','r')
-    con = fs.read()
-    print(con + '\n')
-    fs.close()
-    return con
+
+def pipe_client():
+    print("pipe client")
+    quit = False
+
+    while not quit:
+        try:
+            handle = win32file.CreateFile(
+                r'\\.\pipe\Foo',
+                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                0,
+                None,
+                win32file.OPEN_EXISTING,
+                0,
+                None
+            )
+            res = win32pipe.SetNamedPipeHandleState(
+                handle, win32pipe.PIPE_READMODE_MESSAGE, None, None)
+            if res == 0:
+                print(f"SetNamedPipeHandleState return code: {res}")
+            while True:
+                resp = win32file.ReadFile(handle, 64*1024)
+                # print(resp,"\n")
+                # print(f"message: {resp}\n")
+                output.append(resp)
+        except pywintypes.error as e:
+            if e.args[0] == 2:
+                print("no pipe, trying again in a sec")
+                time.sleep(1)
+            elif e.args[0] == 109:
+                print("broken pipe, bye bye")
+                quit = True
+    return output
 
 
 def main():
@@ -45,15 +80,12 @@ def main():
     for i in record:
         if i != None:
             new_list.append(i)
-            
+
     # f_path =get_template_path()
-    print ("Path returned by Front End is ",get_template_path())
-        
-    
+    print("Path returned by Front End is ", pipe_client())
 
-    print ("Value Fetched from the DB is :",new_list)
-    print ("Value Fetched from the DB Len is :",len(new_list))
-
+    print("Value Fetched from the DB is :", new_list)
+    print("Value Fetched from the DB Len is :", len(new_list))
 
 
 if __name__ == "__main__":
--- index.py
+++ index.py
@@ -1,51 +1,120 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
 from flask_cors import CORS
+import win32pipe
+import win32file
+import time
 import os
+import pyodbc
+
+
+conn = pyodbc.connect(
+    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\DUMA1KOR\Downloads\Database3.accdb;')
+cursor = conn.cursor()
+# cursor.execute('select * from Sheet1 where col_name=Field1')
+cursor.execute('select Field1 from Sheet3')
+
+db_list = []
+
+
+def fetch_data_from_db():
+    records = cursor.fetchall()
+    for record in records:
+        db_list.append(record[0])
+    #     if(record[0].Value=="None"):
+    #         db_list.append(record[0])
+    #         print("Hello World")
+    return db_list
+
 
 app = Flask(__name__)
 CORS(app)
 
 interfaces = []
-fileName = []
 filePath = []
 absPath = []
-resp = []
+# pipe = ""
+abs = []
+
 
 @app.route('/interfaces', methods=['POST'])
 def getInterfaces():
     global interfaces
     interfaces = request.json["interfaces"]
+    print("pipe interfaces")
+    pipe = win32pipe.CreateNamedPipe(
+        r'\\.\pipe\Foo',
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+        1, 65536, 65536,
+        0,
+        None)
+    try:
+        print("waiting for client")
+        win32pipe.ConnectNamedPipe(pipe, None)
+        print("got client")
+
+        win32file.WriteFile(pipe, str.encode(f"{interfaces}"))
+        time.sleep(2)
+        win32file.WriteFile(pipe, str.encode(f"{sendFilePath()}"))
+        abs.clear()
+        # print(sendFilePath())
+        print("finished now")
+    finally:
+        win32file.CloseHandle(pipe)
     return interfaces
+
 
 @app.route('/filePath', methods=['POST'])
 def getFilePath():
     global filePath
-    global resp
-    fileName = request.json["filePath"]
-    # print(fileName)
-    filePath = os.path.abspath(fileName)
-    sendFilePath()
-    resp = make_response(filePath)
-    return filePath
+    global abs
+    filePath = request.json["filePath"]
+    # print(filePath)
+    # print("file executed",os.path.abspath(filePath))
+    abs.append(os.path.abspath(filePath))
+    return os.path.abspath(filePath)
+
 
 def sendFilePath():
-    absPath = filePath
-    print("got abs path", absPath)
-    # open(r'C:\Users\DUMA1KOR\Desktop\Codes\React\Master-TARA-Creator-main\flask\FilePath.txt', 'w').close()
-    with open(r'C:\Users\DUMA1KOR\Desktop\Codes\React\Master-TARA-Creator-main\flask\FilePath.txt', 'a') as f:
-        f.writelines(absPath+'\n')
-    return absPath
-    
+    # global abs
+    # abs.append(os.path.abspath(filePath))
+    # print("got abs path", abs)
+    return abs
+
+
 def getInput():
-    if(len(interfaces) > 0):
+    if (len(interfaces) > 0):
         return "YES"
     else:
         return "NO"
 
+
 @app.route('/checkStatus', methods=['GET'])
 def checkStatus():
+    print("interfaces are", interfaces)
+    # main()
     if getInput() == "YES":
-        print(filePath)
         return "SUCCESS"
     else:
         return "FAILED"
+
+
+def main():
+    print("Hello World")
+    new_list = []
+    record = fetch_data_from_db()
+
+    for i in record:
+        if i != None:
+            new_list.append(i)
+
+    print("selected interfaces are", getInterfaces())
+    # f_path =get_template_path()
+    print("Path returned by Front End is ", sendFilePath())
+
+    # print("Value Fetched from the DB is :", new_list)
+    print("Value Fetched from the DB Len is :", len(new_list))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
