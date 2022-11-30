b61fd011006b07e7330cd1308e9303799af16cae
1900303424 <dumpasai54@outlook.com>
2022-11-30 15:59:23 +0530

    SOCKETS

--- app.py
+++ app.py
@@ -2,27 +2,35 @@ from flask_socketio import *
 from flask import Flask
 from flask_cors import CORS, cross_origin
 import os
+import eventlet
+import socketio
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins='*')
+eventlet.monkey_patch()
+sio = SocketIO(app, cors_allowed_origins='http://localhost:3000', async_mode="threading")
+# sio = socketio.Server()
 
-@socketio.on('interfaces')
-@cross_origin()
+
+@sio.event
 def interfaces(interfaces):
     # socketio.emit("chat", data)
     print("interfaces",interfaces)
-    socketio.emit("checkStatus", "SUCCESS")
+    sio.emit("checkStatus", "SUCCESS")
     return interfaces
 
-@socketio.on('filepath')
-@cross_origin()
-def filepath(filepath):
-    # socketio.emit("chat", data)
-    abspath = os.path.abspath(filepath)
-    print("filepath",abspath)
-    return abspath
+# @sio.event
+# def filepath(filepath):
+#     # socketio.emit("chat", data)
+#     abspath = os.path.abspath(filepath)
+#     print("filepath",abspath)
+#     return abspath
 
+# @sio.event
+# def checkStatus():
+#     status = "SUCCESS"
+#     sio.emit('checkStatus', {'result': status})
+#     print("emitted")
 # @socketio.on('checkStatus')
 # @cross_origin()
 # def checkStatus(status):
@@ -31,5 +39,5 @@ def filepath(filepath):
 #     return status
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    sio.run(app, debug=True)
 
